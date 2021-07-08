4714e7b4c72492a6d6f12e9a57a987705f84503f
Aidas Bendoraitis <aidasbend@yahoo.com>
2021-07-08 02:57:19 +0200

    🔨 More flexible template tags. They now accept either a file object or a file path.

--- image_versions/templatetags/image_versions_tags.py
+++ image_versions/templatetags/image_versions_tags.py
@@ -6,10 +6,18 @@ register = template.Library()
 
 @register.filter
 def original_image_exists(f):
-    return f and default_storage.exists(f.name)
+    """
+    Checks if a file exists in the default storage.
+    f is a file path relative to MEDIA_URL or a file object.
+    """
+    return f and default_storage.exists(f if isinstance(f, str) else f.name)
 
 
 @register.filter
 def to_django_file(f):
-    django_file = default_storage.open(f.name)
+    """
+    Converts f to django.core.files.File object.
+    f is a file path relative to MEDIA_URL or a file object.
+    """
+    django_file = default_storage.open(f if isinstance(f, str) else f.name)
     return django_file
